54c84144b0488f1686de4d46b26c2e0d4beb42e2
Amutha37 <67087939+Amutha37@users.noreply.github.com>
2020-08-17 16:57:33 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-class Calculator {
+class Calculator {                              // USING CLASS TO FOR PREVIOUS AND NEW VALUE
   constructor(preDisplay, curDisplay) {
     this.preDisplay = preDisplay
     this.curDisplay = curDisplay
     this.clear()
   }
 
-  clear() {
+  clear() {                                     // INITIAL CLEARING
     this.curNum = ''
     this.preNum = ''
     this.operation = undefined
   }
-                          // BACKSPACE 
+                                                // BACKSPACE 
   delete() {
     this.curNum = this.curNum.toString().slice(0, -1)
   }
-                                  // VALIDATING DOUBLE DECIMALS 
+                                               // VALIDATING DOUBLE DECIMALS 
   appendNumber(number) {
   //   if (number === '0' && this.preNum ===  "/")
   //    {return(this.currentOperand("ERROR"))
@@ -24,20 +24,11 @@ class Calculator {
     return
     this.curNum = this.curNum.toString() + number.toString()
   
-  }                                                           // DECIDING  THE OPERATION 
+  }                                            // DECIDING  THE OPERATION 
   chooseOperation(operation) {
   
-      // this.operation = operation
-     
-
-      //  if ((this.previousOperand !== '' && this.currentOperand) ===  ("0" && operation === '/')) {
-        
-      //       this.currentOperand = "Err"; return
-          
-      //       }
-          
-        
-      // error 
+      
+                                              // CHECKING FOR VALIDATIN AND SELECTING THE OPERATION
     if (this.curNum === '') return
     if (this.preNum !== '') {
         // If NaN or Infinity returned
@@ -48,7 +39,7 @@ class Calculator {
     this.preNum = this.curNum
     this.curNum = ''
   }
-                              // CALCULATING THE INPUTS 
+                              // CALCULATING THE INPUTS AFTER CONVERTING IT TO NUMERAL VALUE
   calculate() {
     let computation
     const prev = parseFloat(this.preNum)
@@ -91,7 +82,7 @@ class Calculator {
       return integerDisplay
     }
   }
-                              // UPDATING THE PREVIOUS VALUES
+                                 // UPDATING THE PREVIOUS VALUES AND PRINTING VALUES IN THE  DISPLAY 
   updateDisplay() {
     if(this.curNum === Infinity || this.curNum === -Infinity) {
       // console.log(this.currentOperand);
@@ -111,7 +102,7 @@ class Calculator {
   }
 }
 
-//   DECLARING THE ELEMENTS 
+                                                            //   DECLARING THE ELEMENTS 
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -129,24 +120,24 @@ numberButtons.forEach(button => {
     calculator.updateDisplay()
   })
 })
-                                          //  SELECTING OPERATION
+                                                               //  SELECTING OPERATION
 operationButtons.forEach(button => {
   button.addEventListener('click', () => {
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
 })
-                              //EQUAL 
+                                                                //EQUAL 
 equalsButton.addEventListener('click', button => {
   calculator.calculate()
   calculator.updateDisplay()
 })
-
+                                                              // SELECTING CLEARING BUTTON
 allClearButton.addEventListener('click', button => {
   calculator.clear()
   calculator.updateDisplay()
 })
-
+                                                            // DELETING THE DIGIT BACKWARD 
 deleteButton.addEventListener('click', button => {
   calculator.delete()
   calculator.updateDisplay()
